427a344aaf1063aa99150596b6bf6f4cecc9d4bf
LucasReb <lucasreboucas100@gmail.com>
2024-02-26 20:31:58 -0600

    updated getPoll method

--- src/http/routes/get-poll.ts
+++ src/http/routes/get-poll.ts
@@ -3,18 +3,24 @@ import { prisma } from "../../lib/prisma"
 import { FastifyInstance } from "fastify"
 
 export async function getPoll(app: FastifyInstance) {
-    app.get("/poll/:pollId", async (request, reply) => {
+    app.get("/polls/:pollId", async (request, reply) => {
         const getPollParams = z.object({
-            pollId: z.string().uuid()
-        })
-    
+          pollId: z.string(),
+        });
+
         const { pollId } = getPollParams.parse(request.params);
-    
+
         const poll = await prisma.poll.findUnique({
-            where: {
-                id: pollId 
-            }
-        })
+        where: { id: pollId },
+        include: {
+            options: {
+            select: {
+                id: true,
+                title: true,
+            },
+            },
+        },
+        });
 
         return reply.status(200).send({ poll })
     })
--- src/http/server.ts
+++ src/http/server.ts
@@ -1,10 +1,12 @@
 import fastify from "fastify"
 import { createPoll } from "./routes/create-polls"
 import { testAPI } from "./routes/test";
+import { getPoll } from "./routes/get-poll";
 
 const app = fastify();
 
 app.register(createPoll)
+app.register(getPoll)
 app.register(testAPI)
 
 app.listen({port: 3333}).then(() => {
